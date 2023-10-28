1903eeb551632229bc45297833e0cc5db986f40c
Daniel <daniel175depaula@gmail.com>
2023-10-28 13:36:47 -0300

    Importado estruturas de dados

---
+++ src/Pilha/PilhaLista.java
@@ -0,0 +1,51 @@
+package Pilha;
+
+public class PilhaLista<T> implements Pilha<T> {
+
+	private ListaEncadeada<T> lista;
+
+	public PilhaLista() {
+		this.lista = new ListaEncadeada<>();
+	}
+
+	@Override
+	public void push(T info) {
+		lista.inserir(info);
+	}
+
+	@Override
+	public T pop() {
+
+		T valor;
+
+		valor = peek();
+
+		lista.retirar(valor);
+
+		return valor;
+	}
+
+	@Override
+	public T peek() {
+		if (estaVazia()) {
+			throw new PilhaVaziaException();
+		}
+		return lista.getPrimeiro().getInfo();
+	}
+
+	@Override
+	public boolean estaVazia() {
+
+		return lista.estaVazia();
+	}
+
+	@Override
+	public void liberar() {
+		lista = new ListaEncadeada<>();
+	}
+
+	public String toString() {
+		return lista.toString();
+	}
+
+}
---
+++ src/Pilha/PilhaVaziaException.java
@@ -0,0 +1,10 @@
+package Pilha;
+
+public class PilhaVaziaException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
